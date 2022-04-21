d400e34ffd6a0a97ef0b8cb556ea876039c1c3c8
Yu Sang Min <smyu@nate.com>
2022-04-21 23:45:57 +0900

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+
+t.circle(100)      	# 반지름이 100인 원이 그려진다. 
